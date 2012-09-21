2bcbec8292c18ff6f8e4444dd1c2da89a730097c
Roland Walker <walker@pobox.com>
2012-09-21 14:12:16 -0400

    include build and autoload with test target add stub test file no individual tests defined, though

---
+++ ert-tests/minimal-session-saver-test.el
@@ -0,0 +1,20 @@
+
+;; requires and setup
+
+(require 'minimal-session-saver)
+
+;;; this is a stub - no tests defined
+
+;;
+;; Emacs
+;;
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; mangle-whitespace: t
+;; require-final-newline: t
+;; coding: utf-8
+;; byte-compile-warnings: (not cl-functions)
+;; End:
+;;
+
+;;; minimal-session-saver-test.el ends here
