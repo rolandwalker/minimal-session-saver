0870cdf9ef170b3bdc95417595d8a3285751c7fb
Roland Walker <walker@pobox.com>
2012-10-13 22:30:41 -0400

    comment

--- ert-tests/minimal-session-saver-test.el
+++ ert-tests/minimal-session-saver-test.el
@@ -1,9 +1,29 @@
 
-;; requires and setup
+;;; requires and setup
 
 (require 'minimal-session-saver)
 
 ;;; this is a stub - no tests defined
+
+;;; minimal-session-saver-read (path)
+
+;;; minimal-session-saver-write (path file-list)
+
+;;; minimal-session-saver-mkdir-for-file (path)
+
+;;; minimal-session-saver-store (&optional path file-list)
+
+;;; minimal-session-saver-store-frame (&optional path)
+
+;;; minimal-session-saver-load (&optional path)
+
+;;; minimal-session-saver-add-buffer (&optional path buffer)
+
+;;; minimal-session-saver-remove-buffer (&optional path buffer)
+
+;;; minimal-session-saver-mark-stored-buffers (&optional path char col)
+
+;;; minimal-session-saver-install-aliases ()
 
 ;;
 ;; Emacs
